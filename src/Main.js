930db2cf32aea2a1c025461c951d6b851b457ac2
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-02-01 21:46:13 +0300

    Effect hook

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Main(props) {
   const [count, setCount] = useState(0); // takes initial state used in first render
+
+  useEffect(() => {
+    console.log("Mounted");
+    // used to make  side effects
+    // Data fetching, setting up a subscription, and manually changing the DOM in React components are all examples of side effects
+    document.title = count;
+
+    return () => {
+      console.log("Unmounted");
+    };
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div>
       <p>{count}</p>
