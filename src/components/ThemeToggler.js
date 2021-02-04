7bd478f063d55fb0ea78b2f3e2bf1c61cb8c7c44
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-02-04 23:17:18 +0300

    Added theme toggler

---
+++ src/components/ThemeToggler.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ThemeContext from "../context/ThemeContext";
+
+const themeTogglerStyle = {
+  cursor: "pointer",
+};
+
+function ThemeToggler(props) {
+  return <div style={themeTogglerStyle}></div>;
+}
+
+export default ThemeToggler;
