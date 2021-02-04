630086798363d98208ac438d0398efe2b029f134
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-02-04 23:14:01 +0300

    Added header component

---
+++ src/Colors.js
@@ -0,0 +1,11 @@
+const appTheme = {
+  light: {
+    textColor: "#000",
+    backgroundColor: "#fff",
+  },
+  dark: {
+    textColor: "#fff",
+    backgroundColor: "#333",
+  },
+};
+export default appTheme;
